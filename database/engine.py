b9f0e97cc59b49f53eeee64c851e10df26c8308e
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-12-16 01:18:53 +0330

    redis engine added.

--- database/engine.py
+++ database/engine.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from redis import Redis
 
 engine = create_engine("postgresql+psycopg2://mousiol:1234@0.0.0.0:5432/mydb")
 session = Session(engine)
+redis = Redis('0.0.0.0', port=6379)
 
