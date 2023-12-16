abea14c6a6a2d20c04ad8407b715777b5a7ceb2c
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-12-16 15:31:08 +0330

    configuring alembic to migrations.

--- telegram/database/engine.py
+++ telegram/database/engine.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from redis import Redis
 
-engine = create_engine("postgresql+psycopg2://mousiol:1234@0.0.0.0:5432/mydb")
+engine = create_engine(os.environ.get("DB_URL"))
 session = Session(engine)
-redis = Redis('0.0.0.0', port=6379)
+redis = Redis('redis', port=6379)
 
