6d5dcb0d093d0bf49cbd9cbe7ce40e3123b00cd3
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-12-23 00:13:02 +0330

    debug filters.

--- telegram/plugins/filters.py
+++ telegram/plugins/filters.py
@@ -1,8 +1,10 @@
 import re
 from pyrogram import filters
 
+from database.engine import redis
+
 
 def user_cmd_regex(pattern):
     def func(flt, _, message):
-        return re.compile(flt.pattern).match(message.text)
+        return re.compile(flt.pattern).match(redis.get(f"cmd-{message.from_user.id}").decode()) or False
     return filters.create(func, pattern=pattern)
