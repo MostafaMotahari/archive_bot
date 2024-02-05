4fc8d06f49a72f0ad9b14880967c43a1cbb57b3b
Mostafa Motahari <mostafamotahari2004@gmail.com>
2024-02-05 21:59:29 +0330

    encoded share link generator and the share button was added.

--- telegram/plugins/utils.py
+++ telegram/plugins/utils.py
@@ -1,4 +1,5 @@
 import os
+from urllib import parse
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -18,3 +19,13 @@ def cmd_to_path(cmd: str):
             except ValueError:
                 continue
     return os.environ.get('ROOT_DIR') + path
+
+
+def folder_share_link_generator(folder_id: int):
+    with Session(engine) as session:
+        directory = session.scalar(select(Directory).where(Directory.id == folder_id))
+        share_url = f"http://t.me/Qut_archive_Bot?start=dir_{directory.id}"
+        text = f"{share_url}\n\n👆با زدن رو لینک بالا میتونید محتویات فولدر '{directory.persian_title}' رو مشاهده کنید\n🔗 Shared from @Qut_archive_Bot"
+        encoded_text = parse.quote(text)
+        complete_url = f"tg://msg_url?url={encoded_text}"
+        return complete_url
