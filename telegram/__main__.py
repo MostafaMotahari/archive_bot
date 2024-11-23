7a2b36eef7932b4266711009d394d460a3639807
Mostafa Motahari <mostafamotahari2004@gmail.com>
2024-11-23 21:34:39 +0330

    chore(telegram): switch from pyrogram to telethon

--- telegram/__main__.py
+++ telegram/__main__.py
@@ -1,22 +1,13 @@
-from pyrogram import Client
+from telethon import TelegramClient
 import os
-from database.models import Statistics
-from database.engine import engine
-from sqlalchemy.orm import Session
 
 
-app = Client(
-    "Mega archive",
-    api_id=os.environ.get('API_ID'),
+app = TelegramClient(
+    os.environ.get('PROJECT_NAME'),
+    api_id=int(os.environ.get('API_ID')),
     api_hash=os.environ.get('API_HASH'),
-    bot_token=os.environ.get('BOT_TOKEN'),
-    plugins=dict(root="plugins")
+    bot_token=os.environ.get('BOT_TOKEN')
 )
 
-# with Session(engine) as session:
-#     stats = Statistics()
-#     session.add(stats)
-#     session.commit()
-
-
-app.run()
+app.start()
+app.run_until_disconnected()
