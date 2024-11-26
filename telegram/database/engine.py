6d2367b87a1327a1fd7436620624809da90c78b8
Mostafa Motahari <mostafamotahari2004@gmail.com>
2024-11-27 02:24:03 +0330

    chore(db): use async engine for database

--- telegram/database/engine.py
+++ telegram/database/engine.py
@@ -1,9 +1,7 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import create_async_engine
 from redis import Redis
 
-engine = create_engine(os.environ.get("DB_URL"))
-session = Session(engine)
+engine = create_async_engine(os.environ.get("DB_URL"))
 redis = Redis('localhost', port=6379)
 
