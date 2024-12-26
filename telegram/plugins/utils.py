27a5e1ad065e64c7ae8b7d3ad7358178b01a058e
Qut Techs (Mousiol) <quttechs@gmail.com>
2024-12-26 12:05:34 +0330

    feat: create two decorators for antispam check registration.

--- telegram/plugins/utils.py
+++ telegram/plugins/utils.py
@@ -1,57 +1,41 @@
-import os
-from urllib import parse
-
-from telethon.custom import Message
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from functools import wraps
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy import select
+from telethon.custom import Message
 
-from database.models import Directory, Document, BotUser
-from database.engine import engine, redis
-
-
-def cmd_to_path(cmd: str):
-    with Session(engine) as session:
-        directory_ids = cmd.split('/')[:-1]
-        path = ""
-        for directory_id in directory_ids:
-            try:
-                directory = session.scalar(select(Directory).where(Directory.id == int(directory_id)))
-                path += f"{directory.name}/"
-            except ValueError:
-                continue
-    return os.environ.get('ROOT_DIR') + path
+from database.engine import redis, engine
+from database.models import BotUser
 
 
-def folder_share_link_generator(folder_id: int):
-    with Session(engine) as session:
-        directory = session.scalar(select(Directory).where(Directory.id == folder_id))
-        share_url = f"http://t.me/Qut_archive_Bot?start=dir_{directory.id}"
-        text = f"{share_url}\n\n👆با زدن رو لینک بالا میتونید محتویات فولدر '{directory.persian_title}' رو مشاهده کنید\n🔗 Shared from @Qut_archive_Bot"
-        encoded_text = parse.quote(text)
-        complete_url = f"tg://msg_url?url={encoded_text}"
-        return complete_url
+def anti_spam():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(event: Message, *args, **kwargs) -> bool:
+            user_cache_key = 'spam_cache_' + str(await event.from_id.user_id)
+            if redis.get(user_cache_key, None):
+                await event.reply('لطفاً از ارسال پیام‌های تکراری یا غیرمرتبط خودداری کنید.')
+                return False
+            redis.setex(user_cache_key, 1, user_cache_key)
+            return await func(event, *args, **kwargs)
+        return wrapper
+    return decorator
 
 
-def get_absolute_file_path(file: Document):
-    with Session(engine) as session:
-        directory = file.directory
-        path = f"/{file.title}"
-        while True:
-            path = f"/{directory.name}" + path
-            if not directory.parent_id:
-                break
-            directory = session.scalar(select(Directory).where(Directory.id == directory.parent_id))
-        return "/home/archive_bot/docs_repo" + path
+def check_registration():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(event: Message, *args, **kwargs):
+            user_id = str(event.from_id.user_id)
+            if not redis.sismember('cached_user_ids', user_id):
+                async with async_sessionmaker(engine, expire_on_commit=True) as session:
+                    user = BotUser(user_id=user_id)
+                    session.add(user)
+                    session.commit()
+                    redis.sadd('cached_user_ids', user_id)
+            return await func(event, *args, **kwargs)
+        return wrapper
+    return decorator
 
-
-async def anti_spam(event: Message) -> bool:
-    user_cache_key = 'spam_cache_' + str(await event.from_id.user_id)
-    if redis.get(user_cache_key, None):
-        await event.reply('لطفاً از ارسال پیام‌های تکراری یا غیرمرتبط خودداری کنید.')
-        return False
-    redis.setex(user_cache_key, 1, user_cache_key)
-    return True
 
 
 async def load_caches():
