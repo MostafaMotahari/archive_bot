9c4954be4b4962cb6b6676d940e9ea1243d675c8
Qut Techs (Mousiol) <quttechs@gmail.com>
2024-12-26 13:44:34 +0330

    fix: move the client engine to the __main__.py file again.

--- telegram/__main__.py
+++ telegram/__main__.py
@@ -1,5 +1,7 @@
-from client.engine import client
-from plugins import *
+from os import environ
+from telethon import TelegramClient
+
+from plugins import start, panel, inline_search, support
 from plugins.utils import load_caches
 
 # with Session(engine) as session:
@@ -10,4 +12,16 @@ from plugins.utils import load_caches
 # Start-up functions
 # Load caches
 
+
+client = TelegramClient(
+    'QutArchive',
+    api_id=environ.get('API_ID'),
+    api_hash=environ.get('API_HASH')
+)
+
+client.add_event_handler(start.start)
+client.add_event_handler(panel.panel)
+client.add_event_handler(support.support)
+
 client.start()
+client.run_until_disconnected()
--- telegram/client/engine.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from telethon import TelegramClient
-
-
-client = TelegramClient(
-    'QutArchive',
-    api_id=environ.get('API_ID'),
-    api_hash=environ.get('API_HASH')
-)
