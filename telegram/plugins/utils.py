12e49eeb254c021ce7f6e8c49f1a127d7013bfde
Qut Techs (Mousiol) <quttechs@gmail.com>
2024-12-26 13:45:50 +0330

    fix: fix bugs of deacorators.

--- telegram/plugins/utils.py
+++ telegram/plugins/utils.py
@@ -1,40 +1,37 @@
 from functools import wraps
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy import select
-from telethon.custom import Message
+from telethon.events import NewMessage
 
-from database.engine import redis, engine
+from database.engine import redis, engine, session
 from database.models import BotUser
 
 
-def anti_spam():
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(event: Message, *args, **kwargs) -> bool:
-            user_cache_key = 'spam_cache_' + str(await event.from_id.user_id)
-            if redis.get(user_cache_key, None):
-                await event.reply('لطفاً از ارسال پیام‌های تکراری یا غیرمرتبط خودداری کنید.')
-                return False
-            redis.setex(user_cache_key, 1, user_cache_key)
-            return await func(event, *args, **kwargs)
-        return wrapper
-    return decorator
+def anti_spam(func):
+    @wraps(func)
+    async def wrapper(event: NewMessage.Event, *args, **kwargs) -> bool:
+        user_cache_key = 'spam_cache_' + str(await event.get_sender().user_id)
+        if redis.get(user_cache_key, None):
+            await event.reply('لطفاً از ارسال پیام‌های تکراری یا غیرمرتبط خودداری کنید.')
+            return False
+        redis.setex(user_cache_key, 1, user_cache_key)
+        return await func(event, *args, **kwargs)
+    return wrapper
 
 
-def check_registration():
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(event: Message, *args, **kwargs):
-            user_id = str(event.from_id.user_id)
-            if not redis.sismember('cached_user_ids', user_id):
-                async with async_sessionmaker(engine, expire_on_commit=True) as session:
-                    user = BotUser(user_id=user_id)
-                    session.add(user)
-                    session.commit()
-                    redis.sadd('cached_user_ids', user_id)
-            return await func(event, *args, **kwargs)
-        return wrapper
-    return decorator
+def check_registration(func):
+    @wraps(func)
+    async def wrapper(event: NewMessage.Event, *args, **kwargs):
+        user = await event.get_sender()
+        user_id = str(user.id)
+        if not redis.sismember('cached_user_ids', user_id):
+            async with session() as async_session:
+                user = BotUser(user_id=user_id)
+                async_session.add(user)
+                async_session.commit()
+                redis.sadd('cached_user_ids', user_id)
+        return await func(event, *args, **kwargs)
+    return wrapper
 
 
 
