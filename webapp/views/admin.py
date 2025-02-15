ed242c4f2bb7be3ffb8c951c6c5fe6c1abd20659
Mostafa Motahari <mostafamotahari2004@gmail.com>
2025-02-16 02:21:35 +0330

    feat(webapp): add base template and file manager page

--- webapp/views/admin.py
+++ webapp/views/admin.py
@@ -4,4 +4,28 @@ from telegram.database import models
 
 
 class UserAdmin(ModelView, model=models.User):
-    column_list = [models.User.id, models.User.telegram_id]
+    column_list = [models.User.pk, models.User.telegram_id, "uploaded_file_count"]
+
+
+class ModeratorUserAdmin(ModelView, model=models.ModeratorUser):
+    column_list = [models.ModeratorUser.pk, models.ModeratorUser.full_name,
+                   "uploaded_file_count", "verified_files_count"]
+    form_excluded_columns = [models.ModeratorUser.user]
+
+
+class CategoryAdmin(ModelView, model=models.Category):
+    column_list = [models.Category.pk, models.Category.title]
+
+
+class TagAdmin(ModelView, model=models.Tag):
+    column_list = [models.Tag.pk, models.Tag.title]
+
+
+class FileInCloudStorageAdmin(ModelView, model=models.FileInCloudStorage):
+    column_list = [models.FileInCloudStorage.pk, models.FileInCloudStorage.title, models.FileInCloudStorage.directory]
+    form_excluded_columns = [models.FileInCloudStorage.telegram_cloud]
+
+
+class DirectoryAdmin(ModelView, model=models.Directory):
+    column_list = [models.Directory.pk, models.Directory.parent_dir, "files_count"]
+    form_excluded_columns = [models.Directory.files]
