7fb9d73601c92a40dad9408b56306028c9b48936
Mostafa Motahari <mostafamotahari2004@gmail.com>
2025-02-16 02:19:32 +0330

    chore(db): update database url and create async session

--- telegram/database/engine.py
+++ telegram/database/engine.py
@@ -1,7 +1,7 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from redis import Redis
 
 engine = create_async_engine(os.environ.get("DB_URL"))
+async_session: AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
 redis = Redis('localhost', port=6379)
-
