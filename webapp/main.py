0f4172e85c265fd026f818b9cfc128d451a7f397
Mostafa Motahari <mostafamotahari2004@gmail.com>
2025-02-16 02:20:56 +0330

    feat(auth): implement telegram login via hash

--- webapp/main.py
+++ webapp/main.py
@@ -5,22 +5,40 @@ from starlette.applications import Starlette
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.middleware import Middleware
+from starlette.routing import Route
 from sqladmin import Admin
 
 from telegram.database.engine import engine
-from .utils.backends import AdminPanelAuth, login_google
+from .utils.backends import TelegramHashBackend
 from .views import admin as admin_views
+from .views import file_manager as file_manager_views
 
-app = Starlette(debug=True)
-app.add_middleware(SessionMiddleware, secret_key=environ.get("SESSION_SECRET_KEY", "None"), max_age=24*60*60, https_only=False)
+
+routes = [
+    Route("/file_manager_home", endpoint=file_manager_views.file_manager_home, name="file_manager"),
+    Route("/file_manager_alter", endpoint=file_manager_views.file_manager_retrieve, name="file_manager_retrieve_home"),
+    Route("/file_manager_alter/{dir_pk:str}/{tg_id:str}", endpoint=file_manager_views.file_manager_retrieve, name="file_manager_retrieve_sub"),
+]
+
+middleware = [
+    Middleware(SessionMiddleware, secret_key=environ.get("SESSION_SECRET_KEY", "None")),
+    Middleware(AuthenticationMiddleware, backend=TelegramHashBackend()),
+]
+
+app = Starlette(debug=True, routes=routes, middleware=middleware)
+# app.add_middleware(SessionMiddleware, secret_key=environ.get("SESSION_SECRET_KEY", "None"), max_age=24*60*60, https_only=False)
 
 admin_handler = Admin(
-    app,
-    engine,
-    authentication_backend=AdminPanelAuth(secret_key="1234"),
-    title="Qut Archive Admin",
+    app, engine, title="Qut Archive Admin",
     favicon_url=f"http://{environ.get('SITE_URL', 'localhost:8000')}/static/images/logo-no-background.png")
 
 app.router.routes.append(Mount("/static", app=StaticFiles(directory=Path("webapp/static")), name="static"))
+
 admin_handler.add_view(admin_views.UserAdmin)
-admin_handler.app.router.add_route('/auth/google', login_google)
+admin_handler.add_view(admin_views.ModeratorUserAdmin)
+admin_handler.add_view(admin_views.CategoryAdmin)
+admin_handler.add_view(admin_views.TagAdmin)
+admin_handler.add_view(admin_views.FileInCloudStorageAdmin)
+admin_handler.add_view(admin_views.DirectoryAdmin)
--- webapp/utils/backends.py
+++ webapp/utils/backends.py
@@ -1,57 +1,90 @@
-from os import environ
-from typing import Union
+from urllib.parse import unquote, parse_qs
 
-from sqladmin.authentication import AuthenticationBackend
-from authlib.integrations.starlette_client import OAuth
-from starlette.responses import RedirectResponse, Response
+from starlette.middleware.authentication import AuthenticationBackend, AuthCredentials, AuthenticationError
 from starlette.requests import Request
 from sqlalchemy import select
 
-from telegram.database.models import ModeratorUser
-from telegram.database.engine import engine
+from webapp.utils.auth_tools import validate_init_data
+from telegram.database.engine import async_session
+from telegram.database.models import User
 
 
-oauth = OAuth()
-oauth.register(
-    "google",
-    client_id=environ.get("GOOGLE_CLIENT_ID", "None"),
-    client_secret=environ.get("GOOGLE_CLIENT_SECRET", "None"),
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={
-        'scope': 'openid email profile',
-        'prompt': 'select_account',
-    },
-)
-google = oauth.create_client("google")
+class TelegramHashBackend(AuthenticationBackend):
+    async def init_auth(self, request: Request):
+        if not request.query_params: raise AuthenticationError("No initData was fount!")
+
+        if validate_init_data(request.query_params):
+            async with async_session() as session:
+                user_in_db = await session.scalars(select(User).where(User.telegram_id == request.query_params.get('user'))).one()
+                user_in_db.telegram_hash = request.query_params.get('hash')
+                request.sessio["user"] = user_in_db.telegram_hash
+                session.commit()
+                return AuthCredentials(['authenticated']), user_in_db
+        raise AuthenticationError("The hash key is invalid!")
+
+    async def check_auth(self, request: Request, user: str):
+        async with async_session() as session:
+            user_in_db = session.scalars(select(User).where(User.telegram_hash == user)).one() or False
+            if user_in_db:
+                return AuthCredentials(['authenticated']), user_in_db
+            raise AuthenticationError("The hash key was not found!")
+
+    async def authenticate(self, request: Request):
+        user = request.session.get("user", None)
+        if not user:
+            return await self.init_auth(request)
+        return await self.check_auth(request, user)
 
 
-class AdminPanelAuth(AuthenticationBackend):
-    async def login(self, request: Request) -> bool:
-        return True
-
-    async def logout(self, request: Request) -> bool:
-        request.session.clear()
-        return True
-
-    async def authenticate(self, request: Request) -> Union[bool, RedirectResponse]:
-        user = request.session.get('user')
-        print(request.cookies)
-        request.session['kos'] = "Koon"
-        if user:
-            async with engine.connect() as session:
-                moderator_user = await session.scalar(select(ModeratorUser).where(ModeratorUser.email == user["email"]))
-                if moderator_user:
-                    return True if moderator_user.is_active else False
-                return False
-
-        redirect_uri = request.url_for('login_google')
-        return await google.authorize_redirect(request, redirect_uri)
-
-
-async def login_google(request: Request) -> Response:
-    token = await google.authorize_access_token(request)
-    user = token.get('userinfo')
-    request.session['kos'] = "Koon"
-    if user:
-        request.session['user'] = user
-    return RedirectResponse(request.url_for("admin:index"))
+# from sqladmin.authentication import AuthenticationBackend
+# from os import environ
+# from typing import Union
+# 
+# from authlib.integrations.starlette_client import OAuth
+# from starlette.responses import RedirectResponse, Response
+# from telegram.database.models import ModeratorUser, User
+# from telegram.database.engine import engine, async_session
+# oauth = OAuth()
+# oauth.register(
+#     "google",
+#     client_id=environ.get("GOOGLE_CLIENT_ID", "None"),
+#     client_secret=environ.get("GOOGLE_CLIENT_SECRET", "None"),
+#     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+#     client_kwargs={
+#         'scope': 'openid email profile',
+#         'prompt': 'select_account',
+#     },
+# )
+# google = oauth.create_client("google")
+# 
+# 
+# class AdminPanelAuth(AuthenticationBackend):
+#     async def login(self, request: Request) -> bool:
+#         return True
+# 
+#     async def logout(self, request: Request) -> bool:
+#         request.session.clear()
+#         return True
+# 
+#     async def authenticate(self, request: Request) -> Union[bool, RedirectResponse]:
+#         user = request.session.get('user')
+#         print(request.cookies)
+#         request.session['kos'] = "Koon"
+#         if user:
+#             async with engine.connect() as session:
+#                 moderator_user = await session.scalar(select(ModeratorUser).where(ModeratorUser.email == user["email"]))
+#                 if moderator_user:
+#                     return True if moderator_user.is_active else False
+#                 return False
+# 
+#         redirect_uri = request.url_for('login_google')
+#         return await google.authorize_redirect(request, redirect_uri)
+# 
+# 
+# async def login_google(request: Request) -> Response:
+#     token = await google.authorize_access_token(request)
+#     user = token.get('userinfo')
+#     request.session['kos'] = "Koon"
+#     if user:
+#         request.session['user'] = user
+#     return RedirectResponse(request.url_for("admin:index"))
